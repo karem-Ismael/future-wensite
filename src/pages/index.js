11640241a90655bd5833101e68d11ff914b57508
unknown <karem.ismael95@gmail.com>
2023-04-13 06:23:44 +0200

    fix some issues in packages

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,6 @@
 import { Layout, Space,Row,Col,Button} from 'antd';
 import Image from 'next/image'
-import instaImage from "../../public/assets/images/ico-insta.png"
-import twitterImage from "../../public/assets/images/ico-twitter.png"
-import linkedin from "../../public/assets/images/ico-linkedin.png"
+
 import LayoutComponent from '../Components/Layout';
 const App = () => (
 <LayoutComponent>
