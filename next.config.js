18910f7cab451ba5d31bba36f1f7afb9d8da1f5e
ibrahemnour <ebrahimnour186@gmail.com>
2023-10-01 15:09:38 +0200

    fix webback issue

--- next.config.js
+++ next.config.js
@@ -1,7 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.resolve.alias['react-dom'] = 'react-dom'
+    }
+    return config
+  },
+  devServer: (config) => {
+    config.hot = false
+    return config
+  },
 }
 
 module.exports = nextConfig
